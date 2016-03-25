62842723aa68045fbc64833b91e2ed4576e96fac
andrea-83 <andrea@arduino.org>
2016-03-25 16:11:44 +0100

    upgraded rest client example

--- examples/RestClient/RestClient.ino
+++ examples/RestClient/RestClient.ino
@@ -4,11 +4,9 @@ http://labs.arduino.org/RestServer+and+RestClient
 
 */
 
-
 #include <Wire.h>
-#include <Ciao.h>
+#include <ArduinoWiFi.h>
 
-#define CONNECTOR     "rest" 
 #define SERVER_ADDR   "192.168.1.1" // change ip address with your server ip address
 
 int buttonState = LOW; //this variable tracks the state of the button, low if not pressed, high if pressed
@@ -16,41 +14,43 @@ int ledState = -1; //this variable tracks the state of the LED, negative if off,
 long lastDebounceTime = 0;  // the last time the output pin was toggled
 long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
+WifiData espStream;
+ESP espRest(&espStream);
+REST rest(&espRest);
+
 void setup() {
-  Ciao.begin();
+
+  Serial.begin(9600);
   pinMode(2, INPUT);
+  Wifi.begin();
+  rest.begin(SERVER_ADDR);
+  rest.get("/arduino/mode/13/output");
+  delay(1000);
   
 }
  
 void loop() {
- 
+  
   //sample the state of the button - is it pressed or not?
   buttonState = digitalRead(2);
- 
-  //filter out any noise by setting a time buffer
+
   if ( (millis() - lastDebounceTime) > debounceDelay) {
  
     //if the button has been pressed, lets toggle the LED from "off to on" or "on to off"
     if ( (buttonState == HIGH) && (ledState < 0) ) {
- 
-      CiaoData data = Ciao.write(CONNECTOR, SERVER_ADDR, "/arduino/digital/13/1"); //turn LED on
+      
+      rest.begin(SERVER_ADDR);
+      rest.get("/arduino/digital/13/1");      //turn on led
       ledState = -ledState; //now the LED is on, we need to change the state
       lastDebounceTime = millis(); //set the current time
     }
     else if ( (buttonState == HIGH) && (ledState > 0) ) {
       
-      CiaoData data = Ciao.write(CONNECTOR, SERVER_ADDR, "/arduino/digital/13/0"); //turn LED off
+      rest.begin(SERVER_ADDR);
+      rest.get("/arduino/digital/13/0");      //turn off led
       ledState = -ledState; //now the LED is off, we need to change the state
       lastDebounceTime = millis(); //set the current time
-        
-      if (!data.isEmpty()){
-        Ciao.println( "State: " + String (data.get(1)) );
-        Ciao.println( "Response: " + String (data.get(2)) );
-      }
-      else{ 
-        Ciao.println ("Write Error");
-      }
-              
+                     
     }
   }
 
